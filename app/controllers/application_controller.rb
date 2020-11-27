cc0005325667bf7b0bfd2075f0c16ad9613f8816
thiago almeida <thiagopalmeida@gmail.com>
2020-11-27 11:37:40 -0300

    rails incompleto

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
 class ApplicationController < ActionController::Base
-  def task
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'task', to: 'tasks#task'
+
+  # # para ver
+  # get '/tasks', to: 'tasks#index'
+
+  # # para criar
+  # get '/tasks/new', to: 'tasks#new', as: :new
+  # post '/tasks', to: 'tasks#create'
+
+  # # para editar
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # para deletar
+  # delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
+
+  # # para identificar uma task
+  # get '/tasks/:id', to: 'tasks#show', as: :task
+
+  resources :tasks
 end
